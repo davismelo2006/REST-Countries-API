3ddce3644a8bb7d00993dd99467dcc69ebad0610
Davi Melo <davisantosmelo2006@gmail.com>
2022-04-16 16:11:26 -0300

    Filter by region (js)

--- scripts/optionsFilter.js
+++ scripts/optionsFilter.js
@@ -8,34 +8,17 @@ const hideOptions = () => {
 
 drop_options.children[0].addEventListener("click", hideOptions);
 
-allOptions.forEach((el) => {
-  const textValue = el.innerHTML;
-  el.addEventListener("click", () => {
-    drop_options.children[0].children[0].innerHTML = textValue;
-
-    hideOptions();
-    countries_container.innerHTML = getAllCountries(
-      `https://restcountries.com/v3.1/region/${textValue}`
-    );
-  });
-});
-
-const btn_search = search_container.children[0];
-const ipt_search = search_container.children[1];
-
-btn_search.addEventListener("click", () => {
-  countries_container.innerHTML = getAllCountries(
-    `https://restcountries.com/v3.1/name/${ipt_search.value}`
-  );
-});
-
-ipt_search.addEventListener("keydown", (e) => {
-  if (e.code === "Enter" || e.code === "NumpadEnter") {
-    countries_container.innerHTML = getAllCountries(
-      `https://restcountries.com/v3.1/name/${ipt_search.value}`
-    );
-    if (ipt_search.value === "") {
-      getAllCountries(url_all_countries);
+function searchRegion(region) {
+  const newCountryArr = country_arr.filter((el) => {
+    if (el.region === region) {
+      return el;
     }
-  }
+  });
+}
+
+allOptions.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    searchRegion(e.target.innerText);
+    hideOptions();
+  });
 });
