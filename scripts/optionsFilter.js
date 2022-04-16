900cfa24ec3ef4539295b8b329998e819085be78
Davi Melo <davisantosmelo2006@gmail.com>
2022-04-16 19:37:15 -0300

    Search by name

--- scripts/optionsFilter.js
+++ scripts/optionsFilter.js
@@ -2,11 +2,11 @@ const drop_options = document.querySelector(".drop");
 const allOptions = Array.from(drop_options.children[1].children);
 const search_container = document.querySelector(".search-box");
 
-const hideOptions = () => {
+drop_options.addEventListener("click", () => {
   drop_options.children[1].classList.toggle("hidden");
-};
+});
 
-drop_options.children[0].addEventListener("click", hideOptions);
+/* Search by region */
 
 const searchRegion = (region) => {
   const countriesByRegion = countries_arr.filter((el) => {
@@ -20,6 +20,17 @@ allOptions.forEach((el) => {
   el.addEventListener("click", (e) => {
     drop_options.children[0].children[0].innerHTML = e.target.innerText;
     searchRegion(e.target.innerText);
-    hideOptions();
   });
+});
+
+/* Search by name */
+
+search_container.children[1].addEventListener("input", (e) => {
+  const countriesByName = countries_arr.filter((el) => {
+    const name_country = el.name.common.toLowerCase();
+    const ipt_value = e.target.value.toLowerCase();
+    if (name_country.includes(ipt_value)) return el;
+  });
+  countries_container.innerHTML = "";
+  countriesByName.forEach((el) => createCard(el));
 });
