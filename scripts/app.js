39122b0540744eb74a8eb50df2a4636cfe960988
Davi Melo <davisantosmelo2006@gmail.com>
2022-04-17 14:54:12 -0300

    Modal css

--- scripts/app.js
+++ scripts/app.js
@@ -5,21 +5,26 @@ let countries_arr = [];
 const createCard = (el) => {
   let country = document.createElement("div");
   country.classList.add("country-container");
+  country.addEventListener("click", () => {
+    createModal(el);
+  });
 
   country.innerHTML = `
-  <div class="country-flag" style="background-image:url(${
-    el.flags.svg
-  }); "></div>
-<div class="country-desc">
-  <h3> ${el.name.common}</h3>
+    <img src="${el.flags.svg}" alt="country-flag"/>
+  <div class="country-desc">
+   <h3> ${el.name.common}</h3>
   <p><strong>Population:</strong>
-  ${el.population.toLocaleString()}</p>
+      ${el.population.toLocaleString()}</p>
   <p><strong>Region:</strong>
-  ${el.region}</p>
+      ${el.region}</p>
   <p><strong>Capital:</strong>
-  ${el.capital !== undefined ? el.capital[0] : ""}</p>
+      ${el.capital !== undefined ? el.capital[0] : ""}</p>
 </div>`;
   countries_container.appendChild(country);
+};
+
+const createModal = (el) => {
+  console.log(el);
 };
 
 async function getAllCountries() {
@@ -30,4 +35,4 @@ async function getAllCountries() {
   loading_container.style.display = "none";
 }
 
-getAllCountries();
+//getAllCountries();
